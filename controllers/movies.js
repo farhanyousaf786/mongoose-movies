d7188b3b30653edd54cc681c4f03c41c6e090b41
Farhan Yousaf <farhanyousaf1996@gmail.com>
2022-08-06 15:51:03 -0400

    add response of form from db

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,22 +1,35 @@
-const MOVIE = require('../model/movie');
+const Movie = require('../models/movie');
 
 
 module.exports = {
     new: newMovie,
     create,
-
 }
+
 
 function newMovie(req, res,) {
     res.render('movies/new.ejs')
 }
 
+function create(req, res,) {
+    console.log("Content: ", req.body);
+    req.body.nowShowing == !!req.body.nowShowing; // forces value to change to boolean
 
-function create(req, res, ) {
+  // remove whitespace next to commas
+  req.body.cast = req.body.cast.replace(/\s*,\s*/g, ',');
+  // split if it's not an empty string
+  if (req.body.cast) req.body.cast = req.body.cast.split(',');
 
-    console.log("Content: ",  req.body);
-
-    res.send("Response from create function for movie")
 
 
+    Movie.create(req.body,
+        function (err, movieDocumentCreatedInTheDatabase) {
+            if (err) {
+                console.log(err, "< -- Error in movie create controller");
+                return res.render('movies/new.ejs')
+            }
+            console.log(movieDocumentCreatedInTheDatabase, "< -- Created in the databse");
+            // the response is always inside of the callbackof the movie model crud operation
+            res.send("Response from the create function for movies");
+        })
 }
