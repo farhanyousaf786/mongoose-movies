2e2c14bcdb802f90f0ed52e4c4e9e34bb83661bf
Farhan Yousaf <farhanyousaf1996@gmail.com>
2022-08-06 17:09:35 -0400

    renders the object from mongodb

--- controllers/movies.js
+++ controllers/movies.js
@@ -4,7 +4,30 @@ const Movie = require('../models/movie');
 module.exports = {
     new: newMovie,
     create,
+    index,
 }
+
+
+function index(req, res) {
+
+
+    res.send("This is index")
+
+    //List out the movies
+
+    Movie.find({}, function (err, allOfTheMovieInTheDatabase) {
+
+
+        console.log(allOfTheMovieInTheDatabase, "<-- This is allOfTheMovieInTheDatabse")
+        if (err) {
+            res.send("You have error BRO, ")
+        }
+    })
+    res.render('movies/index.ejs', {
+        movies: allOfTheMovieInTheDatabase,
+    });
+}
+
 
 
 function newMovie(req, res,) {
@@ -15,10 +38,10 @@ function create(req, res,) {
     console.log("Content: ", req.body);
     req.body.nowShowing == !!req.body.nowShowing; // forces value to change to boolean
 
-  // remove whitespace next to commas
-  req.body.cast = req.body.cast.replace(/\s*,\s*/g, ',');
-  // split if it's not an empty string
-  if (req.body.cast) req.body.cast = req.body.cast.split(',');
+    // remove whitespace next to commas
+    req.body.cast = req.body.cast.replace(/\s*,\s*/g, ',');
+    // split if it's not an empty string
+    if (req.body.cast) req.body.cast = req.body.cast.split(',');
 
 
 
--- routes/movies.js
+++ routes/movies.js
@@ -5,10 +5,16 @@ const movieController = require('../controllers/movies');
 
 /* GET users listing. */
 
+
+
+
+router.get ('/', movieController.index);
 // -> /movies/new
 router.get('/new', movieController.new);
 
 // -> /movies
 router.post('/', movieController.create);
+
+
 
 module.exports = router;
